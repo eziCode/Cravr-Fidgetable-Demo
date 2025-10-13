ffb20793b49179e400b16cc301dc9a2a5718aac8
Ezra Akresh <ezraakresh@gmail.com>
2025-10-13 07:56:03 -0400

    Initial Commit

---
+++ Cravr Fidgetable Demo/Cravr_Fidgetable_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cravr_Fidgetable_DemoApp.swift
+//  Cravr Fidgetable Demo
+//
+//  Created by Ezra Akresh on 10/13/25.
+//
+
+import SwiftUI
+
+@main
+struct Cravr_Fidgetable_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
